c996c32bd1daf2dc9febe4ad8f8001968ca6bfc1
elhachiiimiii <abdellahhachimi20@gmail.com>
2023-06-15 10:09:17 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
+ * main - Entry point
+ *
+ * Description: Prints a specific string
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
+    return (0);
 }
